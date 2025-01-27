33802edd5196a064bedfca0219bc2ee96b4cd4f4
Csaba Vadász <csabavadasz79@gmail.com>
2025-01-27 19:37:53 +0100

    calling method in Main from MethodSignature and print result

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import method.MethodSignature;
+
 public class Main {
 
     // CTRL + ALT + L -> code formatting (but not solving all issues!)
     public static void main(String[] args) {
 
+        int[] numbers = {1, 2, 3, 4, 5};
+        int result = MethodSignature.multiplyElements(numbers);
+        System.out.println("The result is: " + result);
     }
 }
