73ebf06a32a3655d56c2fc928c6ede781d2a3607
Csaba Vadász <csabavadasz79@gmail.com>
2025-01-29 18:37:31 +0100

    add extra method to FindElementExample.java and call constant in the Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import method.MethodSignature;
 
+import static algorithm.FindElementExample.NUMS;
+
 public class Main {
 
     // CTRL + ALT + L -> code formatting (but not solving all issues!)
@@ -8,5 +10,11 @@ public class Main {
         int[] numbers = {1, 2, 3, 4, 5};
         int result = MethodSignature.multiplyElements(numbers);
         System.out.println("The result is: " + result);
+
+        System.out.println("-----------------");
+        System.out.println("The result is: " + MethodSignature.multiplyElements(numbers));
+        for (int i = 0; i < NUMS.length; i++) {
+            System.out.println("The element at index " + i + " is: " + NUMS[i]);
+        }
     }
 }
--- src/main/java/algorithm/FindElementExample.java
+++ src/main/java/algorithm/FindElementExample.java
@@ -2,17 +2,26 @@ package algorithm;
 
 public class FindElementExample {
 
+    public static final int[] NUMS = {0, 6, 1, 9, 66, 2, 8};
+
     public static void main(String[] args) {
 
         int[] numbers = {3, 6, 1, 9, 66, 2, 8};
         int foundNumber = 66;
+        int notFoundNumber = 100;
+
         int indexOfFoundNumber = findIndexOfElement(numbers, foundNumber);
         System.out.println("The index of the found number is: " + indexOfFoundNumber);
-        int notFoundNumber = 100;
         int indexOfNotfoundNumber = findIndexOfElement(numbers, notFoundNumber);
         System.out.println("The index of the not found number is: " + indexOfNotfoundNumber);
         int min = findMin(numbers);
         System.out.println("The minimum number is: " + min);
+
+        System.out.println("------------------");
+        int[] minExtra = findMinExtra(NUMS);
+        System.out.println("The minimum number is: " + minExtra[0]);
+        System.out.println("The index of the minimum number is: " + minExtra[1]);
+        System.out.println("The sequence of the minimum number is: " + minExtra[2]);
     }
 
     private static int findIndexOfElement(int[] numbers, int number) {
@@ -45,5 +54,23 @@ public class FindElementExample {
         System.out.println("The index of the minimum number is: " + index);
         System.out.println("The sequence of the minimum number is: " + sequence);
         return min;
+    }
+
+    private static int[] findMinExtra(int[] numbers) {
+        int index = 0;
+        int element = index + 1;
+
+        if (numbers == null || numbers.length == 0) {
+            throw new IllegalArgumentException("The array is empty or null!");
+        }
+
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+                index = i;
+            }
+        }
+        return new int[]{min, index, element};
     }
 }
